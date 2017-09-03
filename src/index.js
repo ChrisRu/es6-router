e98bfd8cc1d18bc7eb71c8f484ebbe77789abad3
Christian Ruigrok <christian@ruigrok.info>
2017-09-03 12:09:48 +0200

    Fix indentation TAB to 2 Spaces

--- src/index.js
+++ src/index.js
@@ -1,79 +1,82 @@
 export default class Router {
-    constructor(debug = false) {
-        this.debug = debug;
-        this.routes = [];
+  constructor(debug = false) {
+    this.debug = debug;
+    this.routes = [];
+  }
+
+  add(re, handler) {
+    re = Router.cleanHash(re);
+
+    if (typeof re === 'function') {
+      handler = re;
+      re = new RegExp('');
+    } else if (typeof re === 'string') {
+      re = new RegExp(re);
     }
 
-    add(re, handler) {
-		re = Router.cleanHash(re);
+    this.routes = this.routes.concat({
+      re,
+      handler
+    });
+    return this;
+  }
 
-        if (typeof re === 'function') {
-            handler = re;
-            re = new RegExp('');
-        } else if (typeof re === 'string') {
-			re = new RegExp(re);
-		}
-        
-        this.routes = this.routes.concat({ re, handler });
-        return this;
-    }
+  remove(re) {
+    this.routes = this.routes.filter(route => route.re === re);
+  }
 
-	remove(re) {
-		this.routes = this.routes.filter(route => route.re === re);
-	}
+  check() {
+    const hash = Router.cleanHash(location.hash);
 
-    check() {
-        const hash = Router.cleanHash(location.hash);
-
-        for (let route of this.routes) {
-            const match = hash.match(route.re);
-            if (match !== null && match[0] === hash) {
-                match.shift();
-                route.handler.apply({}, match);
-                if (this.debug) {
-                    console.log(`%c[Router] %cFetching: /${hash}`, 'color: rgb(255, 105, 100);', 'color: inherit');
-                }
-                return this;
-            }
-        }
-        this.navigateError();
-        return this;
-    }
-
-    listen() {
-        this.check();
-        window.addEventListener('hashchange', () => this.check());
-        return this;
-    }
-
-	stopListen() {
-		window.removeEventListener('hashchange', () => this.check());
-		return this;
-	}
-
-    navigate(path) {
+    for (let route of this.routes) {
+      const match = hash.match(route.re);
+      if (match !== null && match[0] === hash) {
+        match.shift();
+        route.handler.apply({}, match);
         if (this.debug) {
-            console.log(`%c[Router] %cRedirecting to: /${Router.cleanHash(path || '')}`, 'color: rgb(255, 105, 100);', 'color: inherit');
+          console.log(`%c[Router] %cFetching: /${hash}`, 'color: rgb(255, 105, 100);', 'color: inherit');
         }
-        history.pushState(null, null, '#/' + Router.cleanHash(path || ''));
         return this;
+      }
     }
+    this.navigateError();
+    return this;
+  }
 
-    navigateError() {
-        if (this.debug) {
-            console.log(`%c[Router] Failed %cFetching: /${hash}, not a valid route.`, 'color: rgb(255, 105, 100);', 'color: inherit');
-        }
-        this.navigate('error');
-    }
+  listen() {
+    this.check();
+    window.addEventListener('hashchange', () => this.check());
+    return this;
+  }
 
-    get currentPage() {
-        return Router.cleanHash(location.hash);
-    }
+  stopListen() {
+    window.removeEventListener('hashchange', () => this.check());
+    return this;
+  }
 
-    static cleanHash(path) {
-        if (path === undefined) {
-            return '';
-        }
-        return path.toString().replace(/^#+\/+|^\/+#+|^\/+|^#+|\/+$|\?(.*)$/g, '');
+  navigate(path) {
+    if (this.debug) {
+      console.log(`%c[Router] %cRedirecting to: /${Router.cleanHash(path || '')}`, 'color: rgb(255, 105, 100);', 'color: inherit');
     }
+    history.pushState(null, null, '#/' + Router.cleanHash(path || ''));
+    return this;
+  }
+
+  navigateError() {
+    if (this.debug) {
+      console.log(`%c[Router] Failed %cFetching: /${hash}, not a valid route.`, 'color: rgb(255, 105, 100);', 'color: inherit');
+    }
+    this.navigate('error');
+  }
+
+  get currentPage() {
+    return Router.cleanHash(location.hash);
+  }
+
+  static cleanHash(path) {
+    if (path === undefined) {
+      return '';
+    }
+    return path.toString().replace(/^#+\/+|^\/+#+|^\/+|^#+|\/+$|\?(.*)$/g, '');
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,37 +1,37 @@
 import Router from './index';
 
 test('Simple routing setup', () => {
-	const router = new Router();
+  const router = new Router();
 
-	router
-		.add(() => {
-			const a = 123;
-		})
-		.add('home', () => {
-			const a = 123;
-		})
-		.listen();
-	
-	window.location.hash = 'home';
+  router
+    .add(() => {
+      const a = 123;
+    })
+    .add('home', () => {
+      const a = 123;
+    })
+    .listen();
 
-	expect(router.currentPage).toEqual('home');
+  window.location.hash = 'home';
+
+  expect(router.currentPage).toEqual('home');
 });
 
 test('Simple routing setup with navigate', () => {
-	const router = new Router();
+  const router = new Router();
 
-	router
-		.add(() => {
-			const a = 123;
-		})
-		.add('home', () => {
-			const a = 123;
-		})
-		.listen();
-	
-	window.location.hash = 'home';
+  router
+    .add(() => {
+      const a = 123;
+    })
+    .add('home', () => {
+      const a = 123;
+    })
+    .listen();
 
-	router.navigate('');
+  window.location.hash = 'home';
 
-	expect(router.currentPage).toEqual('');
+  router.navigate('');
+
+  expect(router.currentPage).toEqual('');
 });
